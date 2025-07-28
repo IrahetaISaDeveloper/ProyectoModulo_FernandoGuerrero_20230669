9834d1845da29708625e733dbaea03249e8bf01f
IrahetaISaDeveloper <20230669@ricaldone.edu.sv>
2025-07-28 11:00:13 -0600

    Creacion de proyecto

---
+++ src/main/java/JavierIraheta_20230669/JavierIraheta_20230669/ProyectoModuloPooJavierIraheta20230669Application.java
@@ -0,0 +1,13 @@
+package JavierIraheta_20230669.JavierIraheta_20230669;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoModuloPooJavierIraheta20230669Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoModuloPooJavierIraheta20230669Application.class, args);
+	}
+
+}
---
+++ src/test/java/JavierIraheta_20230669/JavierIraheta_20230669/ProyectoModuloPooJavierIraheta20230669ApplicationTests.java
@@ -0,0 +1,13 @@
+package JavierIraheta_20230669.JavierIraheta_20230669;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoModuloPooJavierIraheta20230669ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
